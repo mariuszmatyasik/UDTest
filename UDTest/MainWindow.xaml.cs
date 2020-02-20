0376239101ee3327c3d77c537dd5f36f83ad3815
Mariusz Matyasik <mariusz.matyasik@icloud.com>
2020-02-20 23:33:50 +0100

    Dodaj pliki projektów.

---
+++ UDTest/MainWindow.xaml.cs
@@ -0,0 +1,144 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace UDTest
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            var getData = FetchData();
+        }
+
+        public static IList<Testlist> testlists = new List<Testlist>();
+        public static JArray testListModelArray = new JArray();
+
+        public static async Task FetchData()
+        {
+            using (var db = new TestlistDBEntities())
+            {
+                testlists = await (from name in db.Testlists select name).ToListAsync();
+                db.Dispose();
+            }
+        }
+
+        public void PopulateData()
+        {
+            listBox.Items.Clear();
+            
+            foreach (Testlist item in testlists)
+            {
+                listBox.Items.Add(item);
+            }
+        }
+
+        public async Task<IList<Testlist>> UpdateList()
+        {
+            IList<Testlist> lModel = new List<Testlist>();
+            using (var httpClient = new HttpClient())
+                try
+                {
+                    String json = await httpClient.GetStringAsync("http://testyudt.com/UDT_APP/API/index.php?api_key=cdad5e6b5ab66cd4e10b6ace30fee27c&api_tests_list=show");
+                    //  Now parse with JSON.Net
+                    testListModelArray = JArray.Parse(json);
+
+
+                    IList<Testlist> listModel = testListModelArray.Select(p => new Testlist
+                    {
+                        name = (string)p["name"],
+                        description = (string)p["description"],
+                        questionPool = (int)p["question-pool"],
+                        answersLimit = (int)p["answers-limit"],
+                        threshold = (int)p["threshold"],
+                        time = (int)p["time"]
+                    }).ToList();
+
+                    lModel = listModel;
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message, "Connection Error", MessageBoxButton.OK);
+                }
+
+            return lModel;
+        }
+
+        private async void load_Testlist_Click(object sender, RoutedEventArgs e)
+        {
+            var testList = await UpdateList();
+
+                Testlist newItem = new Testlist
+                {
+                    Id = testlists.Count + 1,
+                    name = testList[0].name,
+                    description = testList[0].description,
+                    questionPool = testList[0].questionPool,
+                    answersLimit = testList[0].answersLimit,
+                    threshold = testList[0].threshold,
+                    time = testList[0].time
+                };
+            
+
+            using (var db = new TestlistDBEntities())
+            {
+                var objCtx = ((System.Data.Entity.Infrastructure.IObjectContextAdapter)db).ObjectContext;
+                objCtx.ExecuteStoreCommand("DELETE FROM [Testlist]");
+                await db.SaveChangesAsync();
+                db.Dispose();
+            }
+
+            using (var db = new TestlistDBEntities())
+            {
+                db.Testlists.Add(newItem);
+                await db.SaveChangesAsync();
+                db.Dispose();
+            }
+
+            var newData = FetchData();
+            PopulateData();
+        }
+
+        private void btn_start_test_Click(object sender, RoutedEventArgs e)
+        {
+            var newFrame = new TestFrame(this);
+            newFrame.Show();
+            this.Hide();
+        }
+
+        private void listBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Testlist selected = listBox.SelectedItem as Testlist;
+            
+            if (selected != null)
+            {
+                description_label.Text = selected.description.ToString();
+                q_pool.Content = selected.questionPool.ToString();
+                a_limit.Content = selected.answersLimit.ToString();
+                threshold_nr.Content = selected.threshold.ToString() + "/" + selected.questionPool;
+                time_nr.Content = (selected.time / 60).ToString() + " minut";
+            }
+            else return;
+        }
+
+        private void Window_Closed(object sender, EventArgs e)
+        {
+            System.Windows.Application.Current.Shutdown(1);
+        }
+
+    }
+}
---
+++ UDTest/TestFrame.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace UDTest
+{
+    /// <summary>
+    /// Logika interakcji dla klasy TestFrame.xaml
+    /// </summary>
+    public partial class TestFrame : Window
+    {
+        MainWindow tmp = new MainWindow();
+
+        public TestFrame(MainWindow tmp)
+        {
+            InitializeComponent();
+            this.tmp = tmp;
+        }
+
+        private void Window_Closed(object sender, System.EventArgs e)
+        {
+            tmp.Show();
+        }
+    }
+}
